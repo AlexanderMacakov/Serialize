6becac0bbe6f2a34353553bd74cd06994f7105ca
Alexander Macakov <macakovaleksandr@gmail.com>
2019-02-08 12:49:30 +0400

    init

--- src/Start.java
+++ src/Start.java
@@ -5,10 +5,7 @@ public class Start {
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        int a = -1;
-        if(a % 2 == 1) {
-            System.out.println(1);
-        }
+      
         
     }
 }
